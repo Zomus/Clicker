aadbf155bad9d2327a62b56289373a1fe0025590
SamuelGarm <32417612+SamuelGarm@users.noreply.github.com>
2018-11-17 11:28:14 -0700

    added checkPixelColor function
    finalized the checkPixelColor function in the CanvasController.js

--- server/controllers/CanvasController.js
+++ server/controllers/CanvasController.js
@@ -1,8 +1,10 @@
 
-function checkPixelColor(x = 1, y = 1) {
+function checkPixelColor(x, y) {//gets the RGBA values from a cordinate
+  X = x;
+  Y = y;
 //get the canvasPixelArray from the coordinates
-  var imgd = context.getImageData(x,y,1,1);//x and y are the coordinates you want to check
+  var imgd = ctx.getImageData(X,Y,1,1);//x and y are the coordinates you want to check
   var pix = imgd.data;
 
-  return pix;
+  //console.log(pix)//log the color(debug)
 }
