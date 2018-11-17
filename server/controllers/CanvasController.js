4b1e51b6a0d62740a789f57bbafbf64f08412538
Duncan Lo <duncan.lo@ucalgary.ca>
2018-11-17 16:23:15 -0700

    Added return value and comments to checkPixelColor function.

--- server/controllers/CanvasController.js
+++ server/controllers/CanvasController.js
@@ -2,13 +2,19 @@ module.exports = {
   checkPixelColor: checkPixelColor
 }
 
+/**
+ * Checks the color of a single pixel of a canvas
+ *
+ * @param {Canvas} ctx          Canvas to search
+ * @param {Number} x            Horizontal position of pixel on canvas to check
+ * @param {Number} y            Vertical position of pixel on canvas to check
+ * @returns {int}               Color found in pixel
+ */
 
-function checkPixelColor(x, y) {//gets the RGBA values from a cordinate
-  X = x;
-  Y = y;
-//get the canvasPixelArray from the coordinates
-  var imgd = ctx.getImageData(X,Y,1,1);//x and y are the coordinates you want to check
-  var pix = imgd.data;
+function checkPixelColor(ctx, x, y) {//gets the RGBA values from a cordinate
+  //get the canvasPixelArray from the coordinates
+  var imgd = ctx.getImageData(x, y, 1, 1);
+  //x and y are the coordinates you want to check
 
-  //console.log(pix)//log the color(debug)
+  return imgd.data;
 }
